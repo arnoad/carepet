d29738e20792a1e7df1dbb2667edb40bf44c610f
Paula Gonzalez Navarro <paulezseo@gmail.com>
2023-06-13 16:15:35 +0100

    commiting changes

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,44 @@
 class RequestsController < ApplicationController
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @request = Request.find(params[:id])
+    if @request.update (strong_params)
+      flash[:notice] = 'Booking was successfully updated.'
+      redirect_to bookings_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+  def destroy
+    @request = Request.find(params[:id])
+    @request.destroy
+    redirect_to 
+
 end
