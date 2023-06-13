a26af0fbb28ae8b1f7ab6c1ee3e280e24bdc097a
arno arend <arnoarend@icloud.com>
2023-06-13 12:15:24 +0200

    create seed file, correct type to pet_type, change the validation of default status in the request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,8 +2,16 @@ class Request < ApplicationRecord
   belongs_to :user
   belongs_to :pet
 
+  before_validation :set_default_status
+  
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :carers_home, presence: true
-  validates :status, presence: true, default: 'pending'
+  validates :status, presence: true, inclusion: { in: ['pending', 'rejected', 'accepted'] }
+
+  private
+
+  def set_default_status
+    self.status ||= 'pending'
+  end
 end
