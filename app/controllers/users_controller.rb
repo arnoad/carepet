7b9cd82267aa5cc9ff72f5b5850b63725dc1447c
arno arend <arnoarend@icloud.com>
2023-06-13 15:53:50 +0200

    add show action, rework on the routes, change some file names users > carers

--- app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < Devise::SessionsController
-
-  def index
-    @carers = User.all.where(carer?: true)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # root to: "pages#home"
 
   devise_scope :user do
-    get '/carers', to: 'users#index'
-  end
-  resources :carers do
-    resources :requests, only: %i[new create]
-  end
+    # Defines the root path route ("/")
+    root "carers#index"
 
-  resources :requests, only: %i[show index edit update destroy]
-  resources :pets
+    # get '/carers', to: 'users#index'
+    # get '/carers/:id', to: 'users#show', as: 'carer'
+
+    resources :carers do
+      resources :requests, only: %i[new create]
+    end
+
+    resources :requests, only: %i[show index edit update destroy]
+    resources :pets
+  end
 end
