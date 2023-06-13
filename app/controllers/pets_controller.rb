bfd8b15fe523f4d49396b8f559fb901c2eabbdc6
arno arend <arnoarend@icloud.com>
2023-06-13 21:36:16 +0200

    update action created for pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,6 @@
 class PetsController < ApplicationController
 
-  before_action :find_pet, only: %i[show edit]
+  before_action :find_pet, only: %i[show edit update]
 
   def index
     @pets = current_user.pets
@@ -10,11 +10,21 @@ class PetsController < ApplicationController
 
   def edit; end
 
-  
+  def update
+    if @pet.update(pet_params)
+      redirect_to @pet, flash: { success: 'Details was successfully updated.' }
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def find_pet
     @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :pet_type, :breed, :medical_cond, :special_needs)
   end
 end
