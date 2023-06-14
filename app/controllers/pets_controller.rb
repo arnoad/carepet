24b5acd5ea188b2ff3b26f0141a1fd0659e39c56
arno arend <arnoarend@icloud.com>
2023-06-14 10:43:46 +0200

    add new and create method

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,6 +8,20 @@ class PetsController < ApplicationController
 
   def show; end
 
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pets_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def edit; end
 
   def update
