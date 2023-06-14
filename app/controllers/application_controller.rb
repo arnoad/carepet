83521eef6a506bfec252fae3b5e59ec8193c91f2
Paula Gonzalez Navarro <paulezseo@gmail.com>
2023-06-14 09:37:16 +0100

    removing home navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name address bio house_type carer? age])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name address bio house_type carer? age city])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name address bio house_type carer? age])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name address bio house_type carer? age city])
   end
 end
