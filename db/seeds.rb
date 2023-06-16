e5ed2f50466c339b351ec0f9c15c5000a8b63e85
arno arend <arnoarend@icloud.com>
2023-06-16 12:07:54 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,18 +21,19 @@ puts 'Database cleaned !'
 
 puts 'Create users...'
 
-josh = User.create!(full_name: 'Josh', address: '33 Stendrich 4837 Membach', bio: 'this is my bio', house_type: 'with garden', carer?: false, age: 28, email: 'josh@test.com', password: '123456')
-emma = User.create!(full_name: 'Emma', address: '55 Stendrich 4700 Eupen', bio: 'this is a beautiful bio', house_type: 'flat with balcony', carer?: true, age: 31, email: 'emma@test.com', password: '123456')
-steve = User.create!(full_name: 'Steve', address: '192 Neutralstrasse 4710 Lontzen', bio: 'this is a rich bio', house_type: 'villa', carer?: true, age: 28, email: 'steve@test.com', password: '123456')
+rick = User.create!(full_name: 'Rick Sanchez', address: '420 Queen Street, Brisbane', city: "Brisbane", bio: "I'm Rick Sanchez, the blue-haired genius from 'Rick and Morty.' With my portal gun and sharp intellect, I navigate dimensions, challenge norms, and question the fabric of existence. I may be chaotic and cynical, but deep down, I care about my family... in my own twisted way.", house_type: 'with garden', carer: true, age: 63, email: 'rick@test.com', password: '123456')
+morty = User.create!(full_name: 'Morty Smith', address: '275 George Street, Brisbane', city: "Brisbane", bio: "I'm Morty Smith, the timid yet loyal companion of Rick in 'Rick and Morty.' Navigating bizarre dimensions and facing existential dilemmas alongside Rick, I often find myself out of my comfort zone. Despite my insecurities, I strive to balance adventure with moral compass, growing both intellectually and emotionally.", house_type: 'flat with balcony', carer: true, age: 18, email: 'morty@test.com', password: '123456')
+stan = User.create!(full_name: 'Stan Marsh', address: '140 Creek Street, Brisbane', city: "Brisbane", bio: "I'm Stan Marsh, one of the main characters in 'South Park.' Navigating the absurdities of our town with my friends, I often find myself in hilarious and outrageous situations. As a voice of reason and compassion, I tackle social issues and learn valuable life lessons, all while embracing the quirks and humor of our unique community.", house_type: 'villa', carer: true, age: 28, email: 'stan@test.com', password: '123456')
+
 
 puts 'Users created !'
 
 puts 'Create pets...'
 
-Pet.create!(name: 'Tempo', age: 3, pet_type: 'cat', breed: 'bengal', medical_cond: 'none', special_needs: 'none', user: josh)
-Pet.create!(name: 'Mr Bean', age: 4, pet_type: 'dog', breed: 'labrador', medical_cond: 'none', special_needs: 'none', user: emma)
-Pet.create!(name: 'cartman', age: 6, pet_type: 'bunny', breed: 'dwarf', medical_cond: 'none', special_needs: 'none', user: steve)
-Pet.create!(name: 'remy', age: 2, pet_type: 'rat', breed: 'brown rat', medical_cond: 'none', special_needs: 'good cuisine', user: steve)
+Pet.create!(name: 'Tempo', age: 3, pet_type: 'cat', breed: 'bengal', medical_cond: 'none', special_needs: 'none', user: rick)
+Pet.create!(name: 'Mr Bean', age: 4, pet_type: 'dog', breed: 'labrador', medical_cond: 'none', special_needs: 'none', user: morty)
+Pet.create!(name: 'Cartman', age: 6, pet_type: 'bunny', breed: 'dwarf', medical_cond: 'none', special_needs: 'none', user: stan)
+Pet.create!(name: 'Remy', age: 2, pet_type: 'rat', breed: 'brown rat', medical_cond: 'none', special_needs: 'good cuisine', user: stan)
 
 puts 'Pets created...'
 
