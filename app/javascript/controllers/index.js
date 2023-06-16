3bfb28587160a2c4551fcd7ecb703d4128850d32
Paula Gonzalez Navarro <paulezseo@gmail.com>
2023-06-16 13:28:00 +0100

    implemented search and JS functionality views

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import ViewToggleController from "./view_toggle_controller"
+application.register("view-toggle", ViewToggleController)
