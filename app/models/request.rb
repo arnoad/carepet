9631a16f784ec43cfffd38f2a2ea2a77617be376
arno arend <arnoarend@icloud.com>
2023-06-20 15:26:01 +0200

    repair accept and reject request btn

--- app/models/request.rb
+++ app/models/request.rb
@@ -5,11 +5,11 @@ class Request < ApplicationRecord
   before_validation :set_default_status
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :status, presence: true, inclusion: { in: ['pending', 'rejected', 'accepted'] }
+  validates :status, presence: true, inclusion: { in: ['Pending', 'Rejected', 'Accepted'] }
 
   private
 
   def set_default_status
-    self.status ||= 'pending'
+    self.status ||= 'Pending'
   end
 end
