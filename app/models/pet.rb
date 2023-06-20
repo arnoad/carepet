5be3db56c1555e8a560da64d1778023a048c4139
arno arend <arnoarend@icloud.com>
2023-06-20 12:58:07 +0200

    make pet pic mantadory

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,8 +2,9 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :requests, dependent: :destroy
   has_one_attached :photo
-  
+
   validates :name, presence: true
   validates :age, presence: true
   validates :pet_type, presence: true
+  validates :photo, presence: true
 end
